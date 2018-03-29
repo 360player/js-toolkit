390cb644d9ab2e66815ff57a11fcc3b4ab1c71b9
Robin Grass <hej@carbin.se>
2018-03-29 10:50:43 +0200

    Refactored DateTime to optimize performance.

--- spec/datetime.spec.js
+++ spec/datetime.spec.js
@@ -1,77 +1,14 @@
 /* @dependencies */
-import DateTime from '../src/datetime';
+import DateTime, { Calendar } from '../src/datetime';
 
-/**
- *	@const string PAST_DATE
- */
-const PAST_DATE = '2012-12-12T12:12:12Z';
+describe('Calendar', () => {
 
-/**
- *	@const string FUTURE_DATE
- */
-const FUTURE_DATE = '2222-12-12T12:12:12Z';
+	it('can generate a month', () => {
 
-/**
- *	@const string NOT_LEAP_DATE
- */
-const NOT_LEAP_DATE = '2011-12-12T12:12:12Z';
+		console.time('execTime');
+		console.log( Calendar.generate( 2018, 3 ) );
+		console.timeEnd('execTime');
 
-describe('initial date time values', () => {
-
-	it('defaults to today', () => {
-		const date = new Date();
-		const dateTime = new DateTime();
-
-		expect( DateTime.isToday( date ) ).toBe( true );
-	});
-
-	it('sets initial date', () => {
-		const date = new Date( PAST_DATE );
-		const dateTime = new DateTime( date );
-
-		expect( dateTime.toDate() ).toEqual( date );
-	});
-
-	it('expects a year to be leap year', () => {
-		const date = new Date( PAST_DATE );
-		const dateTime = new DateTime( date );
-
-		expect( dateTime.isLeapYear ).toBe( true );
-	});
-
-	it('expects a year not to be leap year', () => {
-		const date = new Date( NOT_LEAP_DATE );
-		const dateTime = new DateTime( date );
-
-		expect( dateTime.isLeapYear ).toBe( false );
-	});
-
-	it('expects february to have 29 days if leap year', () => {
-		const date = new Date( PAST_DATE );
-		const dateTime = new DateTime( date );
-		dateTime.setMonth( 2 );
-
-		expect( dateTime.daysInMonth ).toBe( 29 );
-	});
-
-	it('expects february to have 28 days if not leap year', () => {
-		const date = new Date( NOT_LEAP_DATE );
-		const dateTime = new DateTime( date );
-		dateTime.setMonth( 2 );
-
-		expect( dateTime.daysInMonth ).toBe( 28 );
-	});
-
-	it('rounds to nearest hour', () => {
-		const date = new Date( FUTURE_DATE );
-		const dateTime = new DateTime( date );
-
-		const controlHour = dateTime.getTime().hour;
-
-		dateTime.toNearestGranularity( 1, 'hour' );
-		const adjustedHour = dateTime.getTime().hour;
-
-		expect( adjustedHour ).toBe( controlHour + 1 );
 	});
 
 });
